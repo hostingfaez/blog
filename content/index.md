8ddb33cc6259f802d9bfc0ea0bbfe01000b167e8
FaezBun <depressedshisuii@gmail.com>
2026-05-31 01:07:21 +0800

    Quartz sync: May 31, 2026, 1:07 AM

--- content/index.md
+++ content/index.md
@@ -22,6 +22,12 @@ A minimal blog and personal knowledge base powered by **Quartz**. This project f
 - [[Linux !]]
 - [[Update]]
 
+
+
+
+
+
+
 <script src="https://giscus.app/client.js"
         data-repo="hostingfaez/blog"
         data-repo-id="R_kgDOSsXGjw"
--- quartz.ts
+++ quartz.ts
@@ -1,21 +1,6 @@
-// If using quartz.ts overrides instead of YAML:
 import { loadQuartzConfig, loadQuartzLayout } from "./quartz/plugins/loader/config-loader"
- 
+
 const config = await loadQuartzConfig()
 export default config
-export const layout = await loadQuartzLayout({
-  defaults: {
-    afterBody: [
-      ExternalPlugin.Comments({
-        provider: "giscus",
-        options: {
-          repo: "hostingfaez/blog",
-          repoId: "R_kgDOSsXGjw",
-          category: "Announcements",
-          categoryId: "DIC_kwDOSsXGj84C-KYL",
-          lang: "en",
-        },
-      }),
-    ],
-  },
-})
+
+export const layout = await loadQuartzLayout()
